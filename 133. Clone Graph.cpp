923bc358d5dbc9bee66a26440c20b7583527e517
Deepak Kumar <deepakku28@gmail.com>
2021-01-21 17:40:48 +0530

    Create 133. Clone Graph.cpp

---
+++ 133. Clone Graph.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    Node* clone(Node* node,unordered_map<int,Node*> &mp){
+        if(mp.find(node->val)!=mp.end()){
+            return mp[node->val];
+        }
+        Node *copy=new Node(node->val);
+        mp[node->val]=copy;
+        for(auto curr:node->neighbors){
+            copy->neighbors.push_back(clone(curr,mp));
+        }
+        return copy;
+    }
+    
+    Node* cloneGraph(Node* node) {
+        if(node==NULL){
+            return NULL;
+        }
+        unordered_map<int,Node*> mp;
+        return clone(node,mp);
+    }
+};

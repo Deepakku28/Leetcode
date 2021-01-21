08ddd4958efc5e6060820968bc9e243893ae94f7
Deepak Kumar <deepakku28@gmail.com>
2021-01-21 17:13:27 +0530

    Create 376. Wiggle Subsequence.cpp

---
+++ 376. Wiggle Subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n=nums.size();
+        if(n<2){
+            return n;
+        }
+        vector<int> up(n,0);
+        vector<int> down(n,0);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]) {
+                    up[i]=max(up[i],down[j]+1);
+                }
+                else if(nums[i]<nums[j]) {
+                    down[i]=max(down[i],up[j]+1);
+                }
+            }
+        }
+        return 1+max(up[n-1],down[n-1]);
+    }
+};

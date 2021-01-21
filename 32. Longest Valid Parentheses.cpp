c6957f3a4369ed1ec9b9e4efd587fe5be114d513
Deepak Kumar <deepakku28@gmail.com>
2021-01-21 19:25:01 +0530

    Create 32. Longest Valid Parentheses.cpp

---
+++ 32. Longest Valid Parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left=0,right=0,max_len=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                left++;
+            }
+            else{
+                right++;
+            }
+            if(left==right){
+                max_len=max(max_len,2*right);
+            }
+            else if(right>left){
+                left=right=0;
+            }
+        }
+        left=right=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='('){
+                left++;
+            }
+            else{
+                right++;
+            }
+            if(right==left){
+                max_len=max(max_len,2*left);
+            }
+            else if(left>right){
+                left=right=0;
+            }
+        }
+        return max_len;
+    }
+};

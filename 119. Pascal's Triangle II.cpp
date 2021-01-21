926812b4257c63be489bcec0b69e910ab827c213
Deepak Kumar <deepakku28@gmail.com>
2021-01-21 14:26:28 +0530

    Create 119. Pascal's Triangle II.cpp

---
+++ 119. Pascal's Triangle II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> ans;
+        for(int i=0;i<=rowIndex;i++){
+            ans.push_back(vector<int>(i+1,1));
+            for(int j=1;j<i;j++){
+                ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+            }
+        }
+        return ans[rowIndex];
+    }
+};

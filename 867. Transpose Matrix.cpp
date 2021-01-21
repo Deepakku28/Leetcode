641df696db0f8c22cae1f4dbfe55a37b957134a9
Deepak Kumar <deepakku28@gmail.com>
2021-01-21 13:30:37 +0530

    Create 867. Transpose Matrix.cpp

---
+++ 867. Transpose Matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        int m=A.size();
+        int n=A[0].size();
+        vector<vector<int>> ans(n,vector<int>(m,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[j][i]=A[i][j];
+            }
+        }
+        return ans;
+    }
+};

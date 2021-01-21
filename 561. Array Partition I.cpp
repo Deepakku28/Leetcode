8c4c750114a661aeb4fa4cc19a0f7f17138180cf
Deepak Kumar <deepakku28@gmail.com>
2021-01-21 11:14:55 +0530

    Create 561. Array Partition I.cpp

---
+++ 561. Array Partition I.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<n;i=i+2){
+            ans+=nums[i];
+        }
+        return ans;
+    }
+};

23ae20f0d67d1940b3ce3cdd7ded864c818170c8
Deepak Kumar <deepakku28@gmail.com>
2021-02-18 08:09:32 +0530

    Create 1237. Find Positive Integer Solution for a Given Equation.cpp

---
+++ 1237. Find Positive Integer Solution for a Given Equation.cpp
@@ -0,0 +1,36 @@
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+        vector<vector<int>> ans;
+        for(int i=1;i<=1000;i++){
+            int left=1;
+            int right=1000;
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(customfunction.f(i,mid)==z){
+                    ans.push_back({i,mid});
+                    break;
+                }
+                else if(customfunction.f(i,mid)>z){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+};

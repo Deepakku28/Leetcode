3c4f58c297b56ab0f44b52a1fbaebe4407744377
Deepak Kumar <deepakku28@gmail.com>
2021-02-18 09:05:10 +0530

    Create 378. Kth Smallest Element in a Sorted Matrix.cpp

---
+++ 378. Kth Smallest Element in a Sorted Matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        int left=matrix[0][0];
+        int right=matrix[n-1][n-1];
+        while(left<right){
+            int mid=left+(right-left)/2;
+            int count=0;
+            int j=n-1;
+            for(int i=0;i<n;i++){
+                while(j>=0 && matrix[i][j]>mid){
+                    j--;
+                }
+                count+=j+1;
+            }
+            if(count<k){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        return left;
+    }
+};

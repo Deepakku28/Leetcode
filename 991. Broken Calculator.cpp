7e44c14ac9e46ce9e031b3530eb82f1d103018c0
Deepak Kumar <deepakku28@gmail.com>
2021-03-01 10:20:14 +0530

    Create 991. Broken Calculator.cpp

---
+++ 991. Broken Calculator.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int ans=0;
+        while(Y>X){
+            if(Y%2==1){
+                Y++;
+            }
+            else{
+                Y/=2;
+            }
+            ans++;
+        }
+        return ans+X-Y;
+    }
+};

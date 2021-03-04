54a40658781ccb1ef8184fea4526decf99a8f544
Deepak Kumar <deepakku28@gmail.com>
2021-03-04 10:02:35 +0530

    Create 590. N-ary Tree Postorder Traversal.cpp

---
+++ 590. N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        stack<Node*> s;
+        s.push(root);
+        while(!s.empty()){
+            Node* curr=s.top();
+            s.pop();
+            ans.push_back(curr->val);
+            for(int i=0;i<curr->children.size();i++){
+                s.push(curr->children[i]);
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
